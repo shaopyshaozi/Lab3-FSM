06a2b65322a64b5b527d818371080a452b857b7f
shaopyshaozi <shaopengyuanspy@outlook.com>
2022-11-03 22:04:16 +0000

    task 1 completed
    successful

---
+++ task1/lfsr4.sv
@@ -0,0 +1,17 @@
+module lfsr4 (
+    input logic             clk,
+    input logic             rst,
+    input logic             en,
+    output logic [3:0]      data_out
+);
+
+logic [3:0]     sreg;
+
+always_ff @ (posedge clk, posedge rst)
+    if (rst)
+        sreg <= 4'b1;
+    else if (en)
+        sreg <= ({sreg[2:0], sreg[3] ^ sreg[2]});
+
+assign data_out = sreg;
+endmodule
